d4c0436de72042a1c2a61bd7d0772064702758f6
Frederic Junod <frederic.junod@camptocamp.com>
2014-11-28 13:28:54 +0100

    snapshot

---
+++ simple.js
@@ -0,0 +1,10 @@
+
+// document.getElementById('layer-selector').layers = map.layers;
+
+// map.viewport.addEventListener('hold', function(event) {
+//   console.log('hold');
+// });
+
+// map.viewport.addEventListener('tap', function(event) {
+//   console.log('tap');
+// });
